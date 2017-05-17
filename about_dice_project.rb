7576d90d9240c05f69ce33238f2363b6ed622988
raj454raj <raj454raj@gmail.com>
2017-05-17 15:40:53 +0530

    All koans done | GREEDGAME left

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -1,10 +1,21 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 # Implement a DiceSet Class here:
-#
-# class DiceSet
-#   code ...
-# end
+
+class DiceSet
+  def initialize
+    @dice_values = []
+  end
+
+  def roll(n)
+    @dice_values = Array.new(n) { rand(1..6) }
+    @dice_values
+  end
+
+  def values
+    @dice_values
+  end
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
